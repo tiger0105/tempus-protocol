176fab7a7cbe7d6b5c84cb89570e47204a2f29d9
Djordje Mijovic <djordje.mijovic@gmail.com>
2021-06-15 13:35:29 +0200

    ci: Adding solidity coverage integration.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 
+require('solidity-coverage');
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (args, hre) => {
