1d35a8fd013551503cf3361332f8990d39e700c9
Alex Beregszaszi <alex@rtfs.hu>
2021-06-17 23:27:16 +0100

    test: Use Aave in Pool tests

--- test/TempusPool.test.ts
+++ test/TempusPool.test.ts
@@ -6,16 +6,25 @@ import { ERC20 } from "./utils/ERC20";
 
 describe("Tempus Pool", async () => {
   let owner, user;
-  let yieldToken:ERC20, pool, principalShare:ERC20, yieldShare:ERC20;
+  let aavePool, yieldToken:ERC20;
+  let pool, principalShare:ERC20, yieldShare:ERC20;
 
   beforeEach(async () => {
     [owner, user] = await ethers.getSigners();
 
     // TODO use actual yield bearing implementation
-    let YieldBearingToken = await ethers.getContractFactory("ERC20FixedSupply");
-    yieldToken = await ERC20.deploy("ERC20FixedSupply", "Yield Bearing Token", "YBT", toWei(1000000));
-    // Pre-fund the user with 10000 tokens
-    await yieldToken.transfer(owner, user, 10000);
+    let BackingToken = await ethers.getContractFactory("ERC20FixedSupply");
+    let aaveBackingAsset = await BackingToken.deploy("DAI Stablecoin", "DAI", toWei(1000000));
+
+    let AavePoolMock = await ethers.getContractFactory("AavePoolMock");
+    aavePool = await AavePoolMock.deploy(aaveBackingAsset.address);
+
+    yieldToken = await ERC20.attach("ATokenMock", await aavePool.yieldToken());
+
+    // Pre-fund the user with 10000 backing tokens, and enter the pool
+    await aaveBackingAsset.connect(owner).transfer(user.address, toWei(10000));
+    await aaveBackingAsset.connect(user).approve(aavePool.address, toWei(1000));
+    await aavePool.connect(user).deposit(aaveBackingAsset.address, toWei(1000), user.address, 0);
 
     let TempusPool = await ethers.getContractFactory("TempusPool");
     // TODO: use block.timestamp
@@ -36,8 +45,8 @@ describe("Tempus Pool", async () => {
   {
     it("Initial exchange rate should be set", async () =>
     {
-      expect(await pool.initialExchangeRate()).to.equal(1);
-      expect(await pool.currentExchangeRate()).to.equal(1);
+      expect(await pool.initialExchangeRate()).to.equal(ethers.utils.parseUnits("1.0", 27));
+      expect(await pool.currentExchangeRate()).to.equal(ethers.utils.parseUnits("1.0", 27));
     });
   });
 
@@ -60,6 +69,22 @@ describe("Tempus Pool", async () => {
       await deposit(100);
       expect(await principalShare.balanceOf(user.address)).to.equal(200);
       expect(await yieldShare.balanceOf(user.address)).to.equal(200);
+    });
+
+    it("Depositing after AAVE increase", async () =>
+    {
+      await yieldToken.approve(user, pool, 200);
+      await deposit(100);
+      expect(await principalShare.balanceOf(user.address)).to.equal(100);
+      expect(await yieldShare.balanceOf(user.address)).to.equal(100);
+
+      await aavePool.connect(owner).setLiquidityIndex("2000000000000000000000000000");
+      await deposit(100);
+      expect(await principalShare.balanceOf(user.address)).to.equal(150);
+      expect(await yieldShare.balanceOf(user.address)).to.equal(150);
+
+      expect(await pool.initialExchangeRate()).to.equal(ethers.utils.parseUnits("1.0", 27));
+      expect(await pool.currentExchangeRate()).to.equal(ethers.utils.parseUnits("2.0", 27));
     });
   });
 });
