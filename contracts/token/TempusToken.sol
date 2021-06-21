693d4c7e46e7b5ced16b51a6b495afe5a52eb1b4
Alex Beregszaszi <alex@rtfs.hu>
2021-06-21 15:14:27 +0100

    f

--- contracts/token/TempusToken.sol
+++ contracts/token/TempusToken.sol
@@ -3,10 +3,10 @@ pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract TempusToken is ERC20("Tempus", "TEMP") {
+contract TempusToken is ERC20 {
     /// TEMP Token constructor, implemented as a fixed supply
     /// @param totalTokenSupply total supply of the token, initially awarded to msg.sender
-    constructor(uint256 totalTokenSupply) {
+    constructor(uint256 totalTokenSupply) ERC20("Tempus", "TEMP") {
         _mint(msg.sender, totalTokenSupply);
     }
 }
