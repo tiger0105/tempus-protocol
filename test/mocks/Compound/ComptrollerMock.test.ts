34f2d0af589936337df4eecc7fea68918ae1fe53
Jorma <jorma.rebane@gmail.com>
2021-06-30 13:59:29 +0300

    test: add basic unit tests for Comptroller

---
+++ test/mocks/Compound/ComptrollerMock.test.ts
@@ -0,0 +1,66 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { Comptroller } from "../../utils/Comptroller";
+import { Signer } from "../../utils/ContractBase";
+
+describe("Compound Mock", async () => {
+  let owner:Signer, user:Signer;
+  let pool: Comptroller;
+
+  beforeEach(async () => {
+    [owner, user] = await ethers.getSigners();
+    pool = await Comptroller.deploy(owner, user, 1000000, 10);
+  });
+
+  describe("Deposit", async () =>
+  {
+    it("Should have 1.0 rate at initial deposit", async () =>
+    {
+      expect(await pool.exchangeRate()).to.equal(1.0);
+      await pool.enterMarkets(user);
+      await pool.payableDeposit(user, 4);
+      
+      expect(await pool.assetBalance(user)).to.equal(6);
+      expect(await pool.yieldBalance(user)).to.equal(4);
+    });
+
+    it("Should receive 0.5x yield tokens if rate is 2.0", async () =>
+    {
+      await pool.setExchangeRate(2.0);
+      expect(await pool.exchangeRate()).to.equal(2.0);
+
+      // with 2.0 rate, user deposits 4 asset tokens and receives 2 yield tokens
+      await pool.enterMarkets(user);
+      await pool.payableDeposit(user, 4);
+
+      expect(await pool.assetBalance(user)).to.equal(6);
+      expect(await pool.yieldBalance(user)).to.equal(2);
+    });
+
+    it("Should receive 2.0x yield tokens if rate is 0.5", async () =>
+    {
+      await pool.setExchangeRate(0.5);
+      expect(await pool.exchangeRate()).to.equal(0.5);
+
+      // with 0.5 rate, user deposits 4 asset tokens and receives 8 yield tokens
+      await pool.enterMarkets(user);
+      await pool.payableDeposit(user, 4);
+
+      expect(await pool.assetBalance(user)).to.equal(6);
+      expect(await pool.yieldBalance(user)).to.equal(8);
+    });
+
+    it("Should receive different amount of yield tokens if rate changes", async () =>
+    {
+      // with 1.0 rate, user deposits 4 assets and receives 4 yield tokens
+      await pool.enterMarkets(user);
+      await pool.payableDeposit(user, 4);
+      expect(await pool.yieldBalance(user)).to.equal(4);
+      
+      // with 2.0 rate, user deposits 4 asset tokens and receives 2 yield tokens
+      await pool.setExchangeRate(2.0);
+      await pool.payableDeposit(user, 4);
+      expect(await pool.yieldBalance(user)).to.equal(6);
+    });
+  });
+});
---
+++ test/utils/Comptroller.ts
@@ -0,0 +1,92 @@
+import { Contract, BigNumber } from "ethers";
+import { NumberOrString, toWei, formatDecimal } from "./Decimal";
+import { ContractBase, SignerOrAddress } from "./ContractBase";
+import { ERC20 } from "./ERC20";
+
+export class Comptroller extends ContractBase {
+  asset: ERC20;
+  earn: ERC20; // yield token
+  
+  constructor(pool: Contract, asset: ERC20, earn: ERC20) {
+    super("AavePoolMock", 18, pool);
+    this.asset = asset;
+    this.earn = earn;
+  }
+
+  /**
+   * @param owner Owner of the pool who has some liquidity
+   * @param user Second user which requires an initial liquidity
+   * @param totalSupply Total DAI supply
+   * @param userBalance How much to transfer to user
+   * @return Deployed instance
+   */
+  static async deploy(owner:SignerOrAddress, user:SignerOrAddress, totalSupply:Number, userBalance:Number): Promise<Comptroller> {
+    // using WEI, because DAI has 18 decimal places
+    const asset = await ERC20.deploy("ERC20FixedSupply", "DAI Stablecoin", "DAI", toWei(totalSupply));
+    const pool = await ContractBase.deployContract("ComptrollerMock", asset.address());
+    const yield_ = await ERC20.attach("CErc20", await pool.yieldToken());
+
+    // Pre-fund the user with backing tokens
+    await asset.transfer(owner, user, userBalance);
+    return new Comptroller(pool, asset, yield_);
+  }
+
+  /**
+   * @return Current Asset balance of the user as a decimal, eg. 1.0
+   */
+  async assetBalance(user:SignerOrAddress): Promise<NumberOrString> {
+    return await this.asset.balanceOf(user);
+  }
+
+  /**
+   * @return Yield Token balance of the user as a decimal, eg. 2.0
+   */
+  async yieldBalance(user:SignerOrAddress): Promise<NumberOrString> {
+    return await this.earn.balanceOf(user);
+  }
+
+  /**
+   * @return Current exchange rate in 1e18 decimal
+   */
+  async exchangeRate(): Promise<NumberOrString> {
+    const rate:BigNumber = await this.contract.exchangeRate();
+    return formatDecimal(rate, 18);
+  }
+
+  /**
+   * Sets the pool exchange rate in 1e18 decimal
+   */
+  async setExchangeRate(exchangeRate:NumberOrString) {
+    await this.contract.setExchangeRate(toWei(exchangeRate));
+  }
+
+  /**
+   * @notice Add assets to be included in account liquidity calculation
+   * @return Success indicator for whether each corresponding market was entered
+   */
+  async enterMarkets(user:SignerOrAddress): Promise<boolean> {
+    const results:BigNumber[] = await this.contract.connect(user).enterMarkets([this.earn.address()]);
+    return results[0] == BigNumber.from("0"); // no error
+  }
+
+  /**
+   * @notice Removes asset from sender's account liquidity calculation
+   * @dev Sender must not have an outstanding borrow balance in the asset,
+   *  or be providing necessary collateral for an outstanding borrow.
+   * @param cTokenAddress The address of the asset to be removed
+   * @return Whether or not the account successfully exited the market
+   */
+  async exitMarket(user:SignerOrAddress): Promise<boolean> {
+    const result:BigNumber = await this.contract.connect(user).exitMarket(this.earn.address());
+    return result == BigNumber.from("0"); // no error
+  }
+
+  /**
+   * Send a payable deposit to the yield token, which will enter us into the pool
+   */
+  async payableDeposit(user:SignerOrAddress, amount:NumberOrString) {
+    await this.asset.approve(user, this.earn.address(), amount);
+    const val = this.earn.toBigNum(amount); // payable call, set value:
+    await this.earn.contract.connect(user).mint({value: val});
+  }
+}
