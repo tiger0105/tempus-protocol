c4d6026069e44b5247c964449c482a1d04cc8521
Jorma <jorma.rebane@gmail.com>
2021-07-05 13:39:27 +0300

    mock: change compound's CTokenInterface to an `interface` from `abstract contract`

--- contracts/mocks/Compound/CTokenInterface.sol
+++ contracts/mocks/Compound/CTokenInterface.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.6;
 
-abstract contract CTokenInterface {
+interface CTokenInterface {
     // as defined in Compound's CTokenInterfaces.sol
-    function exchangeRateCurrent() public view virtual returns (uint);
+    function exchangeRateCurrent() external view returns (uint);
 }
