21411f30c550676b84800facc60983e1f9e56880
Jorma <jorma.rebane@gmail.com>
2021-07-05 13:11:09 +0300

    test: implement currentRate() for IPriceOracle.ts

--- test/utils/IPriceOracle.ts
+++ test/utils/IPriceOracle.ts
@@ -1,4 +1,6 @@
 import { Contract } from "ethers";
+import { ERC20 } from "./ERC20";
+import { NumberOrString } from "./Decimal";
 import { ContractBase } from "./ContractBase";
 
 export class IPriceOracle extends ContractBase {
@@ -8,5 +10,14 @@ export class IPriceOracle extends ContractBase {
   static async deploy(oracleName:string): Promise<IPriceOracle> {
     const oracle = await ContractBase.deployContract(oracleName);
     return new IPriceOracle(oracle, oracleName);
+  }
+  
+  /**
+   * @param token An ERC20 token which belongs to a POOL
+   * @returns Current exchange rate of that Token in the pool
+   */
+  async currentRate(token:ERC20|string): Promise<NumberOrString> {
+    const address:string = (typeof(token) == 'string') ? token : token.address();
+    return this.fromBigNum(await this.contract.currentRate(address));
   }
 }
