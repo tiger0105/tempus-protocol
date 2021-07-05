e155e81bf4939d8acbc3e69a96db873da57c3cca
Djordje Mijovic <djordje.mijovic@gmail.com>
2021-07-05 11:18:55 +0200

    deploy: Adding hardhat-deploy dependancy.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,8 @@ import "hardhat-gas-reporter";
 import "@nomiclabs/hardhat-waffle";
 
 require('solidity-coverage');
+require('hardhat-deploy');
+require("@nomiclabs/hardhat-ethers");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
