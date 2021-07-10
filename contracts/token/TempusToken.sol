6b879a642c9a87e1f52c111d503d56d05726b523
Alex Beregszaszi <alex@rtfs.hu>
2021-07-10 01:20:07 +0100

    token: Simplify TempusToken to use ERC20PresetFixedSupply

--- contracts/token/TempusToken.sol
+++ contracts/token/TempusToken.sol
@@ -1,21 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 
-contract TempusToken is ERC20("Tempus", "TEMP") {
-    /// TEMP Token constructor, implemented as a fixed supply
+/// Tempus Token with a fixed supply and holders having the ability to burn their own tokens.
+contract TempusToken is ERC20PresetFixedSupply {
     /// @param totalTokenSupply total supply of the token, initially awarded to msg.sender
-    constructor(uint256 totalTokenSupply) {
-        _mint(msg.sender, totalTokenSupply);
-    }
-
-    /// Allow token holders to burn their own tokens.
-    /// @param account Token holder account
-    /// @param amount Number of tokens to burn
-    function burn(address account, uint256 amount) external {
-        require(msg.sender == account);
-        require(balanceOf(account) >= amount);
-        _burn(account, amount);
-    }
+    constructor(uint256 totalTokenSupply) ERC20PresetFixedSupply("Tempus", "TEMP", totalTokenSupply, msg.sender) {}
 }
