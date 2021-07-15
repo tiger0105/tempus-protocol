5d619f29be459f59da954d86c54043f69e30f819
Yuval Weiss <yuval.weiss.ace@gmail.com>
2021-07-15 09:09:57 +0200

    lint

--- contracts/mocks/yearn/YearnVaultMock.sol
+++ contracts/mocks/yearn/YearnVaultMock.sol
@@ -14,18 +14,18 @@ contract YearnVaultMock is ERC20, IYearnVaultV2 {
         token = asset;
         totalDebt = 0;
     }
-    
+
     /// @dev Deposits an `amount` of underlying asset into the reserve, receiving Vault Shares in return.
     /// - E.g. User deposits 10 DAI and gets in return X yDai
     /// @param amount The amount to be deposited
-    function deposit(uint256 amount) public override{
+    function deposit(uint256 amount) public override {
         uint256 shares = 0;
         if (totalSupply() == 0) {
             shares = amount;
         } else {
-            shares = (amount * totalSupply() / totalAssets());
+            shares = ((amount * totalSupply()) / totalAssets());
         }
-        
+
         token.transferFrom(msg.sender, address(this), amount);
         _mint(msg.sender, shares);
     }
@@ -43,21 +43,21 @@ contract YearnVaultMock is ERC20, IYearnVaultV2 {
     /// @param targetPrice The desired target price per share
     function increasePricePerShare(uint256 targetPrice) public {
         uint256 totalSharesInCirculation = totalSupply();
-        
+
         require(totalSharesInCirculation > 0, "setting the price is only possible after a deposit was made");
         require(targetPrice > pricePerShare(), "targetPrice must be greater than the current pricePerShare");
-        
+
         totalDebt = (totalSharesInCirculation * targetPrice) / 1e18 - token.balanceOf(address(this));
     }
 
     function shareValue(uint256 shares) internal view returns (uint256) {
         uint256 totalSharesInCirculation = totalSupply();
-        
+
         // Returns price = 1:1 if vault is empty
         if (totalSharesInCirculation == 0) {
             return shares;
         }
 
-        return (shares * totalAssets() / totalSharesInCirculation);
+        return ((shares * totalAssets()) / totalSharesInCirculation);
     }
 }
