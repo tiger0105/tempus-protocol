d193ab9d6ff5c6c6736cc6f18e2b71a31c9055bc
Yuval Weiss <yuval.weiss.ace@gmail.com>
2021-07-15 09:01:53 +0200

    removed hardhat console import; change IYearnVaultV2 licenses to UNLICENSED

--- contracts/mocks/yearn/YearnVaultMock.sol
+++ contracts/mocks/yearn/YearnVaultMock.sol
@@ -1,10 +1,9 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.6;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import "hardhat/console.sol";
 import "./IYearnVaultV2.sol";
 
 contract YearnVaultMock is ERC20, IYearnVaultV2 {
