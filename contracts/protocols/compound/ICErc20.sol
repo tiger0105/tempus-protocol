acaf3124530ebb042ed6c0afec954d81d88192ba
Jorma <jorma.rebane@gmail.com>
2021-07-26 13:21:28 +0300

    lint: satisfy our prettier linter

--- contracts/protocols/compound/ICErc20.sol
+++ contracts/protocols/compound/ICErc20.sol
@@ -5,5 +5,6 @@ import "./ICToken.sol";
 
 interface ICErc20 is ICToken {
     function underlying() external view returns (address);
+
     function mint(uint mintAmount) external returns (uint);
 }
