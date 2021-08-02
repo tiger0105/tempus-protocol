ed8cf6b7a90d47c04ffd79850fae4308e4a74727
Jorma <jorma.rebane@gmail.com>
2021-08-02 15:58:38 +0300

    coverage: remove WadRayMath.sol from coverage

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,6 @@
 module.exports = {
   skipFiles: [
-    'mocks/lido/StETH.sol' // 1:1 with Lido, already tested by upstream
+    'mocks/lido/StETH.sol', // 1:1 with Lido, already tested by upstream
+    'mocks/aave/WadRayMath.sol' // 1:1 with Aave, already tested by upstream
   ]
 };
