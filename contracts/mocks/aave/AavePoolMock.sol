db15555e1db9e095a144649def1c90917e0d8dfe
Jorma <jorma.rebane@gmail.com>
2021-08-02 13:31:42 +0300

    mock: aave withdraw() implemented

--- contracts/mocks/aave/AavePoolMock.sol
+++ contracts/mocks/aave/AavePoolMock.sol
@@ -64,6 +64,33 @@ contract AavePoolMock is ILendingPool {
         yieldToken.mint(onBehalfOf, amountScaled);
     }
 
+    /// @dev Withdraws an `amount` of underlying asset from the reserve, burning the equivalent aTokens owned
+    /// E.g. User has 100 aUSDC, calls withdraw() and receives 100 USDC, burning the 100 aUSDC
+    /// @param asset The address of the underlying asset to withdraw
+    /// @param amount The underlying amount to be withdrawn
+    ///   - Send the value type(uint256).max in order to withdraw the whole aToken balance
+    /// @param to Address that will receive the underlying, same as msg.sender if the user
+    ///   wants to receive it on his own wallet, or a different address if the beneficiary is a
+    ///   different wallet
+    /// @return The final amount withdrawn
+    function withdraw(
+        address asset,
+        uint256 amount,
+        address to
+    ) external override returns (uint256) {
+        require(address(assetToken) == asset, "invalid reserve asset");
+
+        uint256 userBalance = yieldToken.balanceOf(msg.sender);
+        uint256 amountToWithdraw = amount;
+
+        if (amount == type(uint256).max) {
+            amountToWithdraw = userBalance;
+        }
+
+        yieldToken.burn(msg.sender, to, amountToWithdraw, liquidityIndex);
+        return amountToWithdraw;
+    }
+
     /// @notice MOCK ONLY
     /// @return Total deposited underlying assets of an user
     function getDeposit(address user) public view returns (uint) {
