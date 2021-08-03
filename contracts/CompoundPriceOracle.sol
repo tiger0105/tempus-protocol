33b824704ea723032b882a43a476c1db8b58d395
Djordje Mijovic <djordje.mijovic@gmail.com>
2021-08-03 10:16:01 +0200

    fix: Remove TODO for Compound exchange rate in favor of open issue.

--- contracts/CompoundPriceOracle.sol
+++ contracts/CompoundPriceOracle.sol
@@ -7,7 +7,6 @@ import "./protocols/compound/ICToken.sol";
 contract CompoundPriceOracle is IPriceOracle {
     /// @return Current exchange rate as a WAD decimal
     function currentRate(address token) external view override returns (uint256) {
-        // TODO: change this to use exchangeRateCurrent() to avoid unwanted arbitrage
         return ICToken(token).exchangeRateStored();
     }
 
