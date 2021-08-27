afae6e19adcf017b26c989e0a4db05262c14a3e0
Alex Beregszaszi <alex@rtfs.hu>
2021-08-27 19:19:23 +0100

    test: Rework fee test helpers

--- test/utils/TempusPool.ts
+++ test/utils/TempusPool.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { BigNumber, BytesLike, Contract, Transaction } from "ethers";
-import { NumberOrString, toWei } from "./Decimal";
+import { NumberOrString, fromWei, toWei } from "./Decimal";
 import { ContractBase, SignerOrAddress, addressOf } from "./ContractBase";
 import { ERC20 } from "./ERC20";
 import { PoolShare, ShareKind } from "./PoolShare";
@@ -276,7 +276,12 @@ export class TempusPool extends ContractBase {
   }
 
   async getFeesConfig(): Promise<TempusFeesConfig> {
-    return await this.contract.getFeesConfig();
+    let feesConfig = await this.contract.getFeesConfig();
+    return {
+      depositPercent: fromWei(feesConfig.depositPercent),
+      earlyRedeemPercent: fromWei(feesConfig.earlyRedeemPercent),
+      matureRedeemPercent: fromWei(feesConfig.matureRedeemPercent)
+    }
   }
 
   /**
@@ -284,14 +289,12 @@ export class TempusPool extends ContractBase {
    */
   async setFeesConfig(
     owner:SignerOrAddress,
-    depositPercent:NumberOrString,
-    earlyRedeemPercent:NumberOrString,
-    matureRedeemPercent:NumberOrString
+    feesConfig: TempusFeesConfig
   ): Promise<void> {
     await this.contract.connect(owner).setFeesConfig({
-      depositPercent: this.toBigNum(depositPercent),
-      earlyRedeemPercent: this.toBigNum(earlyRedeemPercent),
-      matureRedeemPercent: this.toBigNum(matureRedeemPercent),
+      depositPercent: toWei(feesConfig.depositPercent),
+      earlyRedeemPercent: toWei(feesConfig.earlyRedeemPercent),
+      matureRedeemPercent: toWei(feesConfig.matureRedeemPercent)
     });
   }
 
