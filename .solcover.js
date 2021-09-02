f046021c51fb33eb84ed96e0b4deda6bdcd9f62a
Djordje Mijovic <djordje.mijovic@gmail.com>
2021-09-02 11:30:00 +0200

    Removing files from solccover skip list.

--- .solcover.js
+++ .solcover.js
@@ -2,10 +2,6 @@ module.exports = {
   skipFiles: [
     'mocks/lido/StETH.sol', // 1:1 with Lido, already tested by upstream
     'mocks/aave/WadRayMath.sol', // 1:1 with Aave, already tested by upstream
-    'amm/Vault.sol', // Balancer implementation
-    'amm/TempusAMMFactory.sol', // Balancer implementation
-    'amm/StableMath.sol', // Balancer implementation
-    'amm/TempusAMMUserDataHelpers.sol', // Balancer implementation
-    'amm/Authorizer.sol' // Balancer implementation
+    'amm/TempusAMMFactory.sol' // Balancer implementation
   ]
 };
