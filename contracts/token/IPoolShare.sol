5dae2a922d72a2c77109b0a957382b32c7bdfafd
Yuval Weiss <yuval.weiss.ace@gmail.com>
2021-10-04 06:53:46 -0600

    Fixed circular dependancy

--- contracts/token/IPoolShare.sol
+++ contracts/token/IPoolShare.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.7.6 <0.9.0;
 
-import "../ITempusPool.sol";
-
 /// Interface of Tokens representing the principal or yield shares of a pool.
 interface IPoolShare {
     enum ShareKind {
@@ -14,7 +12,7 @@ interface IPoolShare {
     function kind() external view returns (ShareKind);
 
     /// @return The pool this share is part of.
-    function pool() external view returns (ITempusPool);
+    function pool() external view returns (address);
 
     /// @dev Price per single share expressed in Backing Tokens of the underlying pool.
     ///      This is for the purpose of TempusAMM api support.
