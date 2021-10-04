455f207a29fecd70a313387bd9c54bc1196b7bbc
Yuval Weiss <yuval.weiss.ace@gmail.com>
2021-10-04 09:45:15 -0600

    verify TempusAMM

--- scripts/verify.pool.lido.ts
+++ scripts/verify.pool.lido.ts
@@ -12,6 +12,7 @@ async function tryVerifyingSource(contractName: string, contractAddress: string,
       address: contractAddress,
       constructorArguments: constructorArgs
     });
+    console.log(chalk.green(`Successfully verified ${contractName} on Etherscan - ${contractAddress}`));
   }
   catch (e) {
     if (e.message.includes("Contract source code already verified")) {
@@ -25,12 +26,20 @@ async function tryVerifyingSource(contractName: string, contractAddress: string,
 async function main() {
   console.log(chalk.green("Verifying LidoTempusPool on Etherscan..."));
 
-  const { poolAddress } = await prompt.get(["poolAddress"]);
+  const { tempusAmmAddress } = await prompt.get(["tempusAmmAddress"]);
 
-  const tempusPool = await ethers.getContractAt("LidoTempusPool", poolAddress);  
+  const tempusAMM = await ethers.getContractAt("TempusAMM", tempusAmmAddress);
+  const poolAddress = await tempusAMM.tempusPool();
+  
+  const tempusPool = await ethers.getContractAt("LidoTempusPool", poolAddress);
   const principals = await ethers.getContractAt("PrincipalShare", await tempusPool.principalShare());
   const yields = await ethers.getContractAt("YieldShare", await tempusPool.yieldShare());
 
+  
+  console.log(chalk.yellow("Please enter the constructor required constructor arguments that were used to deploy TempusAMM"));
+  const { pauseWindowDuration, bufferPeriodDuration } = await prompt.get(['pauseWindowDuration', 'bufferPeriodDuration'])
+  
+  console.log(chalk.yellow("Please enter the constructor required constructor arguments that were used to deploy LidoTempusPool"));
   const { 
     estYield,
     depositPercent,
@@ -39,7 +48,7 @@ async function main() {
     referrerAddress
   } = await prompt.get(['estYield', 'depositPercent', 'earlyRedeemPercent', 'matureRedeemPercent', 'referrerAddress'])
   
-  const constructorArgs = await Promise.all([
+  const tempusPoolConstructorArgs = await Promise.all([
     tempusPool.yieldBearingToken(),
     tempusPool.controller(),
     tempusPool.maturityTime(),
@@ -55,8 +64,34 @@ async function main() {
     },
     referrerAddress
   ]);
-  
-  await tryVerifyingSource("LidoTempusPool", poolAddress, constructorArgs);
+
+  const tempusAMMConstructorArgs = await Promise.all([
+    tempusAMM.getVault(),
+    tempusAMM.name(),
+    tempusAMM.symbol(),
+    tempusPool.address,
+    (async () => {
+      const { value, precision } = await tempusAMM.getAmplificationParameter();
+      return value.div(precision);
+    })(),
+    tempusAMM.getSwapFeePercentage(),
+    pauseWindowDuration,
+    bufferPeriodDuration,
+    tempusAMM.owner()
+  ]);
+//   {
+//     vault: "0xBA12222222228d8Ba445958a75a0704d566BF2C8",
+//     name: "Tempus stETH LP Token",
+//     symbol: "TLPstETH",
+//     pool: "0x8A326f00944972F4a218Cc6062D62f093D0490a7",
+//     amplificationParameter: 5,
+//     swapFeePercentage: toWei(0.01),
+//     pauseWindowDuration: 3 * MONTH,
+//     bufferPeriodDuration: MONTH,
+//     owner: "0x6b5975f46a266f58a7a43da2705d34e1998f7299"
+// }
+  await tryVerifyingSource("TempusAMM", tempusAMM.address, tempusAMMConstructorArgs);
+  await tryVerifyingSource("LidoTempusPool", poolAddress, tempusPoolConstructorArgs);
   await tryVerifyingSource("PrincipalShare", principals.address, await Promise.all([tempusPool.address, principals.name(), principals.symbol(), principals.decimals()]));
   await tryVerifyingSource("YieldShare", yields.address, await Promise.all([tempusPool.address, yields.name(), yields.symbol(), yields.decimals()]));
 }
