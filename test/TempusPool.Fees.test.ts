c092b034f5f06f66fb5b702e830bfdbb288b892a
Jorma <jorma.rebane@gmail.com>
2021-10-27 00:18:35 +0300

    test: minor cleanup in TempusPool.Fees test

--- test/TempusPool.Fees.test.ts
+++ test/TempusPool.Fees.test.ts
@@ -12,12 +12,12 @@ describeForEachPool("TempusPool Fees", (pool:ITestPool) =>
 
   beforeEach(async () =>
   {
-    [owner, user, user2] = await ethers.getSigners();
+    await pool.createDefault();
+    [owner, user, user2] = pool.signers;
   });
 
   it("Fee configuration should zero on deployment", async () =>
   {
-    await pool.createDefault();
     let feesConfig = await pool.tempus.getFeesConfig();
     expect(feesConfig.depositPercent).to.equal(0);
     expect(feesConfig.earlyRedeemPercent).to.equal(0);
@@ -26,8 +26,6 @@ describeForEachPool("TempusPool Fees", (pool:ITestPool) =>
 
   it("Fee configuration should be changeable", async () =>
   {
-    await pool.createDefault();
-
     await pool.tempus.setFeesConfig(owner, { depositPercent: 0.15, earlyRedeemPercent: 0.05, matureRedeemPercent: 0.02 });
 
     let feesConfig = await pool.tempus.getFeesConfig();
@@ -38,28 +36,21 @@ describeForEachPool("TempusPool Fees", (pool:ITestPool) =>
 
   it("Fee configuration should revert if deposit percent > max", async () =>
   {
-    await pool.createDefault();
-
     (await expectRevert(pool.tempus.setFeesConfig(owner, { depositPercent: 0.6, earlyRedeemPercent: 0.0, matureRedeemPercent: 0.0 }))).to.be.equal("Deposit fee percent > max");
   });
 
   it("Fee configuration should revert if early redeem percent > max", async () =>
   {
-    await pool.createDefault();
-
     (await expectRevert(pool.tempus.setFeesConfig(owner, { depositPercent: 0.0, earlyRedeemPercent: 1.1, matureRedeemPercent: 0.0 }))).to.be.equal("Early redeem fee percent > max");
   });
 
   it("Fee configuration should revert if mature redeem percent > max", async () =>
   {
-    await pool.createDefault();
-
     (await expectRevert(pool.tempus.setFeesConfig(owner, { depositPercent: 0.0, earlyRedeemPercent: 0.0, matureRedeemPercent: 0.6 }))).to.be.equal("Mature redeem fee percent > max");
   });
 
   it("Should collect tokens as fees during deposit() if fees != 0", async () =>
   {
-    await pool.createDefault();
     await pool.setupAccounts(owner, [[user, 500]]);
 
     await pool.tempus.setFeesConfig(owner, { depositPercent: 0.01, earlyRedeemPercent: 0.0, matureRedeemPercent: 0.0 });
@@ -72,7 +63,6 @@ describeForEachPool("TempusPool Fees", (pool:ITestPool) =>
 
   it("Should collect tokens as fees during EARLY redeem() if fees != 0", async () =>
   {
-    await pool.createDefault();
     await pool.setupAccounts(owner, [[user, 500]]);
 
     await pool.tempus.setFeesConfig(owner, { depositPercent: 0.0, earlyRedeemPercent: 0.01, matureRedeemPercent: 0.0 });
@@ -88,7 +78,6 @@ describeForEachPool("TempusPool Fees", (pool:ITestPool) =>
 
   it("Should collect tokens as fees during MATURE redeem() if fees != 0", async () =>
   {
-    await pool.createDefault();
     await pool.setupAccounts(owner, [[user, 500]]);
 
     await pool.tempus.setFeesConfig(owner, { depositPercent: 0.0, earlyRedeemPercent: 0.0, matureRedeemPercent: 0.02 });
@@ -107,7 +96,6 @@ describeForEachPool("TempusPool Fees", (pool:ITestPool) =>
 
   it("Should collect tokens as fees after maturity with additional yield with fee percentage 0", async () =>
   {
-    await pool.createDefault();
     await pool.setupAccounts(owner, [[user, 500]]);
 
     await pool.depositYBT(user, 100);
@@ -127,7 +115,6 @@ describeForEachPool("TempusPool Fees", (pool:ITestPool) =>
 
   it("Should collect tokens as fees after maturity with additional yield with fee percentage != 0", async () =>
   {
-    await pool.createDefault();
     await pool.setupAccounts(owner, [[user, 500]]);
 
     await pool.tempus.setFeesConfig(owner, { depositPercent: 0.0, earlyRedeemPercent: 0.0, matureRedeemPercent: 0.01 });
@@ -148,7 +135,6 @@ describeForEachPool("TempusPool Fees", (pool:ITestPool) =>
 
   it("Should transfer fees to specified account", async () =>
   {
-    await pool.createDefault();
     await pool.setupAccounts(owner, [[user, 500]]);
 
     await pool.tempus.setFeesConfig(owner, { depositPercent: 0.10, earlyRedeemPercent: 0.0, matureRedeemPercent: 0.0 });
