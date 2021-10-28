2c372261b729d853ea28c684d1b8e3390dcb82a1
Djordje Mijovic <djordje.mijovic@gmail.com>
2021-10-28 12:35:33 +0200

    token: Add option to start inflation.
    Co-authored-by: Alex Beregszaszi <alex@rtfs.hu>

--- test/utils/TempusToken.ts
+++ test/utils/TempusToken.ts
@@ -1,6 +1,8 @@
 import { NumberOrString } from "./Decimal";
 import { SignerOrAddress, addressOf } from "./ContractBase";
 import { ERC20 } from "./ERC20";
+import { Signer } from "crypto";
+import { Transaction } from "@ethersproject/transactions";
 
 /**
  * Type safe wrapper of TempusToken
@@ -20,12 +22,38 @@ export class TempusToken extends ERC20 {
   }
 
   /**
-   * Burn some other token holder's tokens.
-   * @param sender Account that is issuing the burn.
-   * @param account Token holder account
-   * @param amount Number of tokens to burn
+   * Mint some tokens
+   * @param sender Account that is allowed to mint tokens
+   * @param account Account to which we issue tokens
+   * @param amount Number of tokens to mint
    */
-  async burnFrom(sender:SignerOrAddress, account:SignerOrAddress, amount:NumberOrString): Promise<void> {
-    await this.connect(sender).burnFrom(addressOf(account), this.toBigNum(amount));
+  async mint(sender:SignerOrAddress, account:SignerOrAddress, amount:NumberOrString): Promise<Transaction> {
+    return this.connect(sender).mint(addressOf(account), this.toBigNum(amount));
+  }
+
+  /**
+   * @returns The timestamp after which minting is allowed
+   */
+  async mintingAllowedAfter(): Promise<number> {
+    return this.contract.mintingAllowedAfter();
+  }
+
+  /**
+   * @returns The timestamp of last minting occurance
+   */
+  async lastMintingTime(): Promise<number> {
+    return this.contract.lastMintingTime();
+  }
+
+  async MIN_TIME_BETWEEN_MINTS(): Promise<number> {
+    return this.contract.MIN_TIME_BETWEEN_MINTS();
+  }
+
+  async INITIAL_SUPPLY(): Promise<NumberOrString> {
+    return this.contract.INITIAL_SUPPLY();
+  }
+
+  async MINT_CAP(): Promise<NumberOrString> {
+    return this.contract.MINT_CAP();
   }
 }
