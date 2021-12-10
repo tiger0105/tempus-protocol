4070b02f5a2010df9d8179ae9241ddb504eb6742
Djordje Mijovic <djordje.mijovic@gmail.com>
2021-12-10 14:14:47 +0100

    Add proper license to all contracts that were unlicensed.

--- contracts/ITempusPool.sol
+++ contracts/ITempusPool.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity >=0.7.6 <0.9.0;
 pragma abicoder v2;
 
--- contracts/protocols/compound/IComptroller.sol
+++ contracts/protocols/compound/IComptroller.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.10;
 
 // Based on https://github.com/compound-finance/compound-protocol/blob/v2.8.1/contracts/ComptrollerInterface.sol
