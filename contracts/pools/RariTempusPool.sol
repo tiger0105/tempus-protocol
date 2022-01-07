e814ecc6fd062ec81f29045f8168ee9a554c12c2
Alex Beregszaszi <alex@rtfs.hu>
2022-01-07 12:38:59 +0000

    pools: Use assert(msg.value == 0) in depositUnderlying in Rari

--- contracts/pools/RariTempusPool.sol
+++ contracts/pools/RariTempusPool.sol
@@ -71,7 +71,8 @@ contract RariTempusPool is TempusPool {
     }
 
     function depositToUnderlying(uint256 amount) internal override returns (uint256) {
-        require(msg.value == 0, "ETH deposits not supported");
+        // ETH deposits are not accepted, because it is rejected in the controller
+        assert(msg.value == 0);
 
         // Deposit to Rari Pool
         IERC20(backingToken).safeIncreaseAllowance(address(rariFundManager), amount);
